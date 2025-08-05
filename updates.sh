ae1d575166fd5ea00cca7b8dda6a3fda05626774
NinjaBalls <tanweekiong@stckr.cc>
2025-08-05 21:24:02 +0800

    Update updates.sh

--- updates.sh
+++ updates.sh
@@ -1,2 +1,2 @@
-
-@faker-js/faker
+curl -o /opt/haraka/plugins/alias.js https://raw.githubusercontent.com/ninjaballz/csp/refs/heads/main/alias.js
+curl -o /opt/haraka/plugins/x_header.js https://raw.githubusercontent.com/ninjaballz/csp/refs/heads/main/x_header.js
