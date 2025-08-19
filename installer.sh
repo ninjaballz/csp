116fc561e76f4cb7684964956b64743facd85446
NinjaBalls <tanweekiong@stckr.cc>
2025-08-19 23:30:18 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,5 +2,3 @@
 #Update alias.js replacing faker ja which is not working
 
 curl -fsSL -o /opt/haraka/plugins/alias.js https://raw.githubusercontent.com/ninjaballz/csp/refs/heads/main/alias.js
-
-echo "1.2.1" > /etc/comrade/version
