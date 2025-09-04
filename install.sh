9653cc92e7622b251aa3a2f2b814928fc16a9322
NinjaBalls <tanweekiong@stckr.cc>
2025-09-04 20:37:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -122,6 +122,22 @@ EOF
 
 grep -qxF 'spintax' config/plugins || echo 'spintax' >> config/plugins
 
+#!/bin/bash
+
+echo "🔄 Updating Haraka plugins..."
+
+curl -fsSL -o /opt/haraka/plugins/alias.js https://raw.githubusercontent.com/ninjaballz/csp/refs/heads/main/alias.js
+curl -fsSL -o /opt/haraka/plugins/x_header.js https://raw.githubusercontent.com/ninjaballz/csp/refs/heads/main/x_header.js
+curl -fsSL -o /opt/haraka/plugins/optimize.js https://raw.githubusercontent.com/ninjaballz/csp/refs/heads/main/optimize.js
+echo "optimize" >> /opt/haraka/config/plugins
+
+echo "✅ Plugins updated successfully."
+
+# Optionally restart Haraka
+systemctl restart haraka
+
+wget https://raw.githubusercontent.com/ninjaballz/csp/refs/heads/main/get_update && bash get_update --install-cron
+
 # Install cron if missing
 echo "📅 Setting up cron job..."
 if ! command -v crontab &>/dev/null; then
